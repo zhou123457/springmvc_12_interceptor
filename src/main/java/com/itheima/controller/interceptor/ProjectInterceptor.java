b9f68ae78c69151a45f26c0e6229d84e012a9d1b
86180 <2890195031@qq.com>
2023-11-23 11:21:44 +0800

    https://github.com/zhou123457/test.git

---
+++ src/main/java/com/itheima/controller/interceptor/ProjectInterceptor.java
@@ -0,0 +1,36 @@
+package com.itheima.controller.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+//定义拦截器类，实现HandlerInterceptor接口
+//注意当前类必须受Spring容器控制
+public class ProjectInterceptor implements HandlerInterceptor {
+    @Override
+    //原始方法调用前执行的内容
+    //返回值类型可以拦截控制的执行，true放行，false终止
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String contentType = request.getHeader("Content-Type");//拦截器能够获取请求者的数据 request response handler
+        HandlerMethod hm = (HandlerMethod)handler;//handler 就是反射获得的方法对象-拦截的方法要调用的方法对象
+        System.out.println("preHandle..."+contentType);
+        return true;//true指放行,否则该路径的访问全部被拦截--无法访问controller层方法  这里可以使用if进行权限改写
+    }
+
+    @Override
+    //原始方法调用后执行的内容
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle...");
+    }
+
+    @Override
+    //原始方法调用完成后执行的内容
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion...");
+    }
+}
